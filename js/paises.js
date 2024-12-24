07cd06b5146665e8a4caaa2f62ef0b2c8db13fef
letciahs <leticiasoareshaussman@gmail.com>
2024-12-24 03:14:01 +0000

    implementacao do codigo para o user poder colocar favorito e retirar favorito na pagina paises

--- js/paises.js
+++ js/paises.js
@@ -3,7 +3,7 @@ var copiarCartaoOriginal = $('#cartao').clone(); //variavel para copiar e guarda
 
 
 // função que vai carregar os paises. Recebe o URl para fazer o get da API / Foi preciso criar em vez de se usar o Document.reade
-// Assim poupamos linhas de código e reutilizamos o que temos.
+// Assim poupamos linhas de código e reutilizamos o que temos
 
 function carregarPaises(url){
 
@@ -15,8 +15,9 @@ function carregarPaises(url){
 
         success: function(campos){//se for success a funcao vai receber o array campos com nome, bandeira, populacao e capital
             console.log(campos);
+            //transformo os favcoritos em um objeto json e se nao tiver nenhum favorito cria um array vazio
+            let favoritos = JSON.parse(localStorage.getItem('favoritos')) || [];
             // esse for vai gerar cards ate o tamanho do campo.length
-
             for (let i = 0; i < campos.length; i++) {
                 const bandeira=campos[i].flags.png; // como acessar campos dentro do array 
                 const nome=campos[i].name.common;
@@ -28,11 +29,52 @@ function carregarPaises(url){
                 $('.card-capital', copiarCartao).text("Capital: "+capital);
                 $('.card-population', copiarCartao).text("População: "+populacao);
                 $('#cartao-container').append(copiarCartao);//envio o codigo do card gerado para o container
+
+                      //guardar os dados do país em um dicionario para enviar ao localStorage
+                      const dadosPais = {
+                        bandeira: bandeira,
+                        nome: nome,
+                        populacao: populacao,
+                        capital: capital
+    
+                    };
+                    const botaoFavorito = $('.favorite', copiarCartao);
+                    // se houver um objeto favoritos.nome que é igual a const nome retorna true
+                    //https://www.w3schools.com/jsref/jsref_some.asp
+                    //some é um metodo booleano do js
+                    const isFavorite = favoritos.some(favoritos=>favoritos.nome === nome);
+
+                    // o .css e o .html sao metodos do jquery para fazer a manipulacao do estilo
+                    if (isFavorite) {
+                        botaoFavorito.css('color', 'yellow');
+                        botaoFavorito.html('&#9733');
+                    } else {
+                        botaoFavorito.css('color', '');
+                        botaoFavorito.html('&#9734');
+                    }
+
+                    //o metodo on é um metodo jquery
+                    botaoFavorito.on('click',function(){
+                        if (favoritos.some(favorito => favorito.nome === nome)) {
+                            //https://www.w3schools.com/jsref/jsref_filter.asp
+                            //atualizando removendo da lista de favoritos
+                            favoritos=favoritos.filter(favoritos=>favoritos.nome !== nome);
+                            botaoFavorito.css('color', '');
+                            botaoFavorito.html('&#9734');
+                        }else{
+                            //adicionando favoritos
+                            favoritos.push(dadosPais);
+                            botaoFavorito.css('color', 'yellow');
+                            botaoFavorito.html('&#9733');
+                        }
+                        localStorage.setItem('favoritos', JSON.stringify(favoritos));
+                    });
             }
         },
         error: function(){
             console.log('Erro ao carregar campos.');//se der erro ao carregar os dados dos campos vai aparecer essa mensagem
         }
+
 
     });
 }
@@ -55,4 +97,3 @@ $('#btn-search').on('click', function(){
         carregarPaises("https://restcountries.com/v3.1/name/"+ valorPesquisa+"?fields=name,flags,population,capital");
     }
 });
-
